d7359532040532d52368d505f3bf0631b541d2f3
Dylan Jones <dylanjones1608@gmail.com>
2025-04-10 08:58:18 +0100

    getting started - publishing step

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+// Image switcher
+
 const myImage = document.querySelector('img');
 
 myImage.onclick = function() {
@@ -8,3 +10,29 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/d20.png');
     }
   }
+
+  // Personalised dice roller title
+
+  let myButton = document.querySelector("button");
+  let myHeading = document.querySelector("h1");
+
+  function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
+
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `${storedName}'s Dice Roller`;
+  }
+
+  myButton.addEventListener("click", () => {
+    setUserName();
+  });
